063881e1d6cfb82c11386309e98fade836bc2337
ashi72 <32313598+ashi72@users.noreply.github.com>
2022-03-11 13:22:37 -0800

    moved important database login info out into a .env file

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
 const URI =
-  "mongodb+srv://dbUserA:bruin123@cluster35l.x01ed.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://"+user+":"+password+"@cluster35l.x01ed.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const connectDB = async () => {
   await mongoose.connect(
     URI,

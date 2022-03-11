e22c8ac6cc34b61e4689bfb8ae1146a133cf4c7b
JJPolarbear <75065075+jjpolarbear@users.noreply.github.com>
2022-03-11 02:58:18 -0800

    Fix Review routing, add search capabilities to catalog
    - Fix Review routing since it was being called with "review" instead of "Review"
    - Add search bar to product catalog
    - Filter products on catalog such that their name must contain search query

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,17 +5,40 @@ import productAPI from "../services/products";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     productAPI.getAll().then((products) => {
-      setProducts(products);
+      setProducts(
+        products.filter((product) => {
+          const productName = product?.name?.toLowerCase();
+          // TODO: Remove condition once database has been flushed
+          if (productName) return productName.includes(search);
+          return true;
+        })
+      );
     });
-  }, []);
+  }, [search]);
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value.toLowerCase());
+  };
+
   return (
     <>
       <div className="hero is-primary">
         <div className="hero-body container">
           <h4 className="title">Our Products</h4>
         </div>
+      </div>
+      <br />
+      <div className="searchbar">
+        <input
+          type="text"
+          placeholder="search for items here"
+          name="searchbarname"
+          value={search}
+          onChange={handleSearchChange}
+        />
       </div>
       <br />
       <div className="container">
